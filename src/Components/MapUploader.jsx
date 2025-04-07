e16cfec727e5e4880c17cdb4fa2c991da1071dcf
Shivamk-Nickelfox <shivam.k@nickelfox.com>
2025-04-07 18:23:54 +0530

    Fully Resposive

--- src/Components/MapUploader.jsx
+++ src/Components/MapUploader.jsx
@@ -3,11 +3,44 @@ import { useState } from "react";
 import { DOMParser as Dom } from "xmldom";
 import * as toGeoJSON from "@tmcw/togeojson";
 import { FaFolderOpen } from "react-icons/fa";
-
+import { Box, IconButton, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useMap } from "react-leaflet";
+import React, { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 
 const MapUploaderKML = () => {
   const [geoData, setGeoData] = useState(null);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
+
+  let marginLeftValue;
+
+  if (isSmallScreen) {
+    marginLeftValue = "0";
+  } else if (isTablet) {
+    marginLeftValue = "-5px";
+  } else if (isDesktop) {
+    marginLeftValue = "-10px";
+  } else {
+    marginLeftValue = "-7px";
+  }
+  const ResizeHandler = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      const handleResize = () => {
+        map.invalidateSize();
+      };
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, [map]);
+
+    return null;
+  };
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -31,49 +64,66 @@ const MapUploaderKML = () => {
   };
 
   return (
-    <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+      mt={4}
+      position="relative"
+    >
+      <Box
+        sx={{
+          width: {
+            xs: "95vw",
+            sm: "90vw",
+            md: "80vw",
+            lg: "70vw",
+          },
+          height: {
+            xs: "70vh",
+            sm: "80vh",
+            md: "85vh",
+            lg: "90vh",
+          },
+          position: "relative",
         }}
       >
+        <IconButton
+          component="label"
+          sx={{
+            position: "absolute",
+            top: 78,
+            left: 16,
+            zIndex: 1000,
+            ml: marginLeftValue,
+            backgroundColor: "white",
+            "&:hover": { backgroundColor: "#f0f0f0" },
+          }}
+        >
+          <FaFolderOpen size={24} color="#0000FF" />
+          <input type="file" accept=".kml" onChange={handleFileUpload} hidden />
+        </IconButton>
+
         <MapContainer
           center={[20, 0]}
           zoom={2}
           style={{
-            height: "900px",
-            width: "1000px",
+            height: "100%",
+            width: "100%",
+            borderRadius: "8px",
+            zIndex: 0,
           }}
         >
-          <label
-            style={{
-              zIndex: 1000,
-              position: "absolute",
-              top: 75,
-              left: 10,
-              cursor: "pointer",
-              marginLeft: "5px",
-            }}
-          >
-            <FaFolderOpen size={30} color="#333" />
-            <input
-              type="file"
-              accept=".kml"
-              onChange={handleFileUpload}
-              style={{ display: "none" }}
-            />
-          </label>
+          <ResizeHandler />
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; OpenStreetMap contributors"
           />
           {geoData && <GeoJSON data={geoData} />}
         </MapContainer>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
